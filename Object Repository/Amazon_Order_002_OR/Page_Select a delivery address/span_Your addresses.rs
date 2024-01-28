74373253c8d76e19dfccdbc2ef1a4264dabbcde6
ganta <ganta@LAPTOP-7O4HO4AH>
2024-01-28 16:48:35 +0530

    last push

---
+++ Object Repository/Amazon_Order_002_OR/Page_Select a delivery address/span_Your addresses.rs
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Your addresses</name>
+   <tag></tag>
+   <elementGuidId>2a95d73b-0f93-46a0-9ee2-da0fa7529d96</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span.a-text-bold</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='address-list']/div/div/div/fieldset/h4/div/div/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>31afb6c0-45c4-4811-930e-58d399874ae0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-text-bold</value>
+      <webElementGuid>55f7fd9f-9c7b-4600-a8bd-7dc1dfb251a3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+          Your addresses
+        </value>
+      <webElementGuid>5a8a1d74-787a-46b3-aa4d-ca2d4d0d7a6a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;address-list&quot;)/div[@class=&quot;a-box-group a-spacing-small&quot;]/div[@class=&quot;a-box&quot;]/div[@class=&quot;a-box-inner&quot;]/fieldset[1]/h4[1]/div[@class=&quot;a-row&quot;]/div[@class=&quot;a-column a-span5 a-text-left&quot;]/span[@class=&quot;a-text-bold&quot;]</value>
+      <webElementGuid>8763e910-8f91-4cd9-aa1c-36d2be534d03</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='address-list']/div/div/div/fieldset/h4/div/div/span</value>
+      <webElementGuid>5f318e7b-9496-4a92-ba13-2f2c4c00588e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h4/div/div/span</value>
+      <webElementGuid>78d20911-c598-4a79-94e8-d36a604b1ad2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = '
+          Your addresses
+        ' or . = '
+          Your addresses
+        ')]</value>
+      <webElementGuid>dbe90000-f5db-4daf-aa1d-7c605a8a568f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
